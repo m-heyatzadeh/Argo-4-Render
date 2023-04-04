4890e52fa48b9b8c32b63e96d19b272a3595ae08
m-heyatzadeh <67802861+m-heyatzadeh@users.noreply.github.com>
2023-04-04 22:49:27 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const username = process.env.WEB_USERNAME || "admin";
-const password = process.env.WEB_PASSWORD || "password";
+const username = process.env.WEB_USERNAME || "ar89";
+const password = process.env.WEB_PASSWORD || "AR.89@89n";
 const url = process.env.RENDER_EXTERNAL_URL;
 const port = process.env.PORT || 3000;
 const express = require("express");
